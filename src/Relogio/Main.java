3b9e45c52df54777085d0105297361f94e8ac400
Ian Nascimento Rocha <ian18nr@gmail.com>
2023-08-29 16:19:25 -0300

    codigo classe main e Relogio

--- src/Relogio/Main.java
+++ src/Relogio/Main.java
@@ -1,5 +1,53 @@
 package Relogio;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Relogio relogio = new Relogio();
+        Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.println("\nMenu:");
+            System.out.println("1. Definir horário");
+            System.out.println("2. Reiniciar para meia-noite");
+            System.out.println("3. Iniciar cronômetro");
+            System.out.println("4. Imprimir horário (formato 24 horas)");
+            System.out.println("5. Imprimir horário (formato AM/PM)");
+            System.out.println("6. Sair");
+            System.out.print("Digite sua escolha: ");
+            
+            int escolha = scanner.nextInt();
+            
+            switch (escolha) {
+                case 1:
+                    System.out.print("Digite as horas: ");
+                    int horas = scanner.nextInt();
+                    System.out.print("Digite os minutos: ");
+                    int minutos = scanner.nextInt();
+                    System.out.print("Digite os segundos: ");
+                    int segundos = scanner.nextInt();
+                    relogio.definirHorario(horas, minutos, segundos);
+                    break;
+                case 2:
+                    relogio.reiniciarMeiaNoite();
+                    break;
+                case 3:
+                    relogio.iniciarCronometro();
+                    break;
+                case 4:
+                    relogio.imprimirHorario(true);
+                    break;
+                case 5:
+                    relogio.imprimirHorario(false);
+                    break;
+                case 6:
+                    System.out.println("Saindo...");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Escolha inválida. Por favor, selecione uma opção válida.");
+                    break;
+            }
+        }
+    }
 }
--- src/Relogio/Relogio.java
+++ src/Relogio/Relogio.java
@@ -1,5 +1,96 @@
 package Relogio;
 
-public class Relogio {
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Scanner;
 
+class Relogio {
+    private int horas;
+    private int minutos;
+    private int segundos;
+
+    public Relogio() {
+        this.horas = 0;
+        this.minutos = 0;
+        this.segundos = 0;
+    }
+    
+    
+
+    public int getHoras() {
+		return horas;
+	}
+
+
+
+	public void setHoras(int horas) {
+		this.horas = horas;
+	}
+
+
+
+	public int getMinutos() {
+		return minutos;
+	}
+
+
+
+	public void setMinutos(int minutos) {
+		this.minutos = minutos;
+	}
+
+
+
+	public int getSegundos() {
+		return segundos;
+	}
+
+
+
+	public void setSegundos(int segundos) {
+		this.segundos = segundos;
+	}
+
+
+
+	public void definirHorario(int horas, int minutos, int segundos) {
+        this.horas = horas;
+        this.minutos = minutos;
+        this.segundos = segundos;
+    }
+
+    public void reiniciarMeiaNoite() {
+        this.horas = 0;
+        this.minutos = 0;
+        this.segundos = 0;
+    }
+
+    public void iniciarCronometro() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Pressione Enter para iniciar o cronômetro...");
+        scanner.nextLine();
+
+        long horaInicio = System.currentTimeMillis();
+
+        System.out.print("Pressione Enter novamente para parar o cronômetro...");
+        scanner.nextLine();
+
+        long horaFim = System.currentTimeMillis();
+        long millisDecorridos = horaFim - horaInicio;
+
+        int segundosDecorridos = (int) (millisDecorridos / 1000);
+        int minutosDecorridos = segundosDecorridos / 60;
+        segundosDecorridos %= 60;
+
+        System.out.println("Tempo decorrido do cronômetro: " + minutosDecorridos + " minutos " + segundosDecorridos + " segundos");
+    }
+
+    public void imprimirHorario(boolean formato24Horas) {
+        LocalTime horarioAtual = LocalTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(formato24Horas ? "HH:mm:ss" : "hh:mm:ss a");
+        String horarioFormatado = horarioAtual.format(formatter);
+        System.out.println("Horário atual: " + horarioFormatado);
+    }
 }
+
