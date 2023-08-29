02f5d40fa2dbc6967b57d34654206f7ff6a62a2b
Ian Nascimento Rocha <ian18nr@gmail.com>
2023-08-29 15:49:39 -0300

    testando

---
+++ test/Relogio/RelogioTest.java
@@ -0,0 +1,14 @@
+package Relogio;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RelogioTest {
+
+	@Test
+	void verificarNumero() {
+		Relogio relogio = new Relogio ();
+	}
+
+}
