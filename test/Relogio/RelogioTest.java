4cf533785c9a4fb39099c3699831f181fb0955c2
Ian Nascimento Rocha <ian18nr@gmail.com>
2023-08-29 16:17:39 -0300

    Testes relogio

--- test/Relogio/RelogioTest.java
+++ test/Relogio/RelogioTest.java
@@ -7,8 +7,14 @@ import org.junit.jupiter.api.Test;
 class RelogioTest {
 
 	@Test
-	void verificarNumero() {
+	void verificarHora() {
 		Relogio relogio = new Relogio ();
+		relogio.setHoras(1);
+		//verificar se a hora esta igual a informada
+		assertEquals(1, relogio.getHoras());
+		
+		
+		 
 	}
 
 }
