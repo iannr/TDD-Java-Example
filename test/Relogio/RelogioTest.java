a9415c5f893e25559544f23e74e55e7e050c3ed8
Ian Nascimento Rocha <ian18nr@gmail.com>
2023-08-30 14:18:48 -0300

    teste funcionando

--- test/Relogio/RelogioTest.java
+++ test/Relogio/RelogioTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class RelogioTest {
-
+// primeiro teste
 	@Test
 	void verificarHora() {
 		Relogio relogio = new Relogio ();
@@ -18,3 +18,4 @@ class RelogioTest {
 	}
 
 }
+
